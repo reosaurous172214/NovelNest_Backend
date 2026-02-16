214f2ef2d1c20780332e3dc9ede5f49709ab55aa
reosaurous172214 <saurabhsharma7061@gmail.com>
2026-02-16 11:44:53 +0530

    Final production build with global error handling

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -259,7 +259,7 @@ export const sendOTP = async (req, res) => {
 
     res.status(200).json({ success: true, message: "OTP sent!" });
   } catch (error) {
-    res.status(500).json({ message: err});
+    res.status(500).json({ message: error});
   }
 };
 
